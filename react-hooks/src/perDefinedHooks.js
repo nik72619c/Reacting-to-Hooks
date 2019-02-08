7a21b372a5d8a6e23d5c2982679d5fc22e71de8a
nik72619c <nikhilsharmarockstar21@gmail.com>
2019-02-08 20:07:13 +0530

    useReducer

--- react-hooks/src/perDefinedHooks.js
+++ react-hooks/src/perDefinedHooks.js
@@ -1,6 +1,22 @@
-import React,{useState,useEffect,useContext} from 'react';
+import React,{useState,useEffect,useContext,useReducer} from 'react';
+let initialState={
+    counter:0
+}
+function reducer(state=initialState,action){
+  switch(action.type){
+    case 'increment': return {counter: state.counter+1};
+    break;
+    case 'decrement': return {counter: state.counter-1};
+    break;
+    default: return {status: 'not defined'}
+    break;
+  }
+}
 
 const PreDefined=() =>{
+    var [state,dispatch]=useReducer(reducer,initialState);
+    console.log('state', state);
+    console.log('dispatch',dispatch);
 
     var [count,setCount]=useState(0);
     var [count2,setCount2]=useState(0);
@@ -17,8 +33,11 @@ const PreDefined=() =>{
     return (
         <>
         <div>you clicked {count} times</div>
+        Count: {state.counter}
         <button onClick={()=>setCount(count+1)}>click me!</button>
         <button onClick={()=>setCount2(count2+1)}>click me 2!</button>
+        <button onClick={()=>dispatch({type: 'increment'})}>+</button>
+        <button onClick={()=>dispatch({type: 'decrement'})}>-</button>
          </>
     )
 };
